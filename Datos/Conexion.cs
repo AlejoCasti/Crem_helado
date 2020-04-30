166b60972987822bfea5d5df6d17b085caa662c9
AlejoCasti <37104762+AlejoCasti@users.noreply.github.com>
2020-04-29 20:24:32 -0500

    [ADD] Data directory

---
+++ Datos/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Datos
+{
+    public class Conexion
+    {
+
+        public SqlConnection conexion()
+        {
+            SqlConnection conexión = new SqlConnection("data source = CRISTIAN-PC\\SQLEXPRESS;Database =Crem_Helado;Integrated Security = true");
+            return conexión;
+        }
+    }
+}
